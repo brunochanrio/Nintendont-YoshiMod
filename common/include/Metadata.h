4a25790fc24b7f37d1d39aa4b2aa4f24d1980ba5
Bruno-chanrio <16856395+brunochanrio@users.noreply.github.com>
2025-06-16 17:04:45 -0300

    Update Metadata.h

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -2,10 +2,10 @@
 #ifndef __META_DATA_H__
 #define __META_DATA_H__
 
-#define META_NAME			"Nintendont"
-#define META_AUTHOR			"FIX94, crediar"
+#define META_NAME			"Nintendont Yoshi's Mod"
+#define META_AUTHOR			"FIX94, crediar, YoshiDeLana2025"
 
-#define META_LONG1			"Commiters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan \r\n\r\n Project website: https://github.com/FIX94/Nintendont "
+#define META_LONG1			"Nintendont mod by YoshiDeLana2025 \r\n\r\n Commiters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan \r\n\r\n Project website: https://github.com/FIX94/Nintendont "
 #define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
 #define META_SHORT			"Gamecube Loader"
 
